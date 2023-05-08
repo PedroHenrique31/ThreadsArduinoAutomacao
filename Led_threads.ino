8911587091e22635a00585d95e2fb2a7a3cf5aca
Pedro Henrique Carneiro de Araújo <pedrohcaraujo1@hotmail.com>
2023-05-08 20:53:18 -0300

    Fiz umas mudancinhas no codigo para melhorar a legibilidade e velocidade no codigo, mas o funcionamento permanece o mesmo.

--- Led_threads.ino
+++ Led_threads.ino
@@ -2,8 +2,13 @@
   Autor: Pedro Henrique Cerneiro de Araújo        RA:22108287
   Autor: Bruna Stephanie Soares Gonçalves         RA:21707921
 */
+// saídas analógicas para o Led
+#define pinoR 6
+#define pinoG 5
+#define pinoB 3
 
-int pinoR=6,pinoG=5,pinoB=3; // saídas analógicas para o Led
+//int pinoR=6,pinoG=5,pinoB=3;
+int valoresPinos[]={128,199,64}; 
 void setup() {
   pinMode(pinoR,OUTPUT);
   pinMode(pinoG,OUTPUT);
@@ -12,10 +17,16 @@ void setup() {
 }
 
 void loop() {
-  int valoresPinos[]={128,199,64};
+  //static int valoresPinos[]={128,199,64};// pode ser mais interessante fazer disso uma variável global, apesar de feioso.
 
   analogWrite(pinoR,valoresPinos[0]);
   analogWrite(pinoG,valoresPinos[1]);
   analogWrite(pinoB,valoresPinos[2]);
+
+  valoresPinos[0]=random(0,256);
+  valoresPinos[1]=random(0,256);
+  valoresPinos[2]=random(0,256);
+
+  delay(500);
 
 }
